3d4e7764b55c1ba7ed3a4ac343d3e119ab62d4fc
Ryan Hutchison <rwhutchison@live.com>
2018-11-07 21:39:07 -0600

    Print x, y, z positions to debugger

--- xyz_accel/ViewController.swift
+++ xyz_accel/ViewController.swift
@@ -7,18 +7,26 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var xLabel: UILabel!
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
+    
+    var motionManager: CMMotionManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
     }
-
-
+    
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else { return }
+        print(accelerometerData)
+    }
 }
 
